c7d6a6a8d6ca7b1094e736cb804e66eb30ed5716
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-18 22:40:12 -0300

    implement add/update

--- Aula9/js/script.js
+++ Aula9/js/script.js
@@ -1,25 +1,72 @@
 window.addEventListener('load', start);
+var names = [];
 
 function start() {
     var form = document.querySelector('form');
-    form.addEventListener('submit', preventDefault);
+    form.addEventListener('load', preventDefault);
+    form.addEventListener('submit', addNameInArray);
+
+}
+
+function updateElementsInHtml() {
+    let div = document.querySelector('.list');
+    div.innerHTML = '';
+
+    for (let index = 0; index < names.length; index++) {
+
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        let button = document.createElement('button');
+        let text = document.createElement('h5');
+        text.setAttribute('class', 'btn-text');
+        text.textContent = names[index];
+        button.setAttribute('class', 'btn');
+        button.textContent = 'X';
+        button.addEventListener('click', removeNameInArray);
+        text.addEventListener('click', editValue);
+
+        li.append(button);
+        li.append(text);
+        ul.append(li);
+        div.append(ul);
+
+    }
+
+
+}
+
+function editValue(event) {
+    preventDefault(event);
+    removeNameInArray(event);
+}
+
+function setInputValue(value) {
+    let input = document.querySelector('#name');
+    input.value = value;
+}
+
+function getInputValue() {
+    let input = document.querySelector('#name');
+    return input.value;
+}
+
+function addNameInArray(event) {
+    preventDefault(event);
+    names.push(getInputValue());
+    updateElementsInHtml();
+}
+
+
+function removeNameInArray(event) {
+    preventDefault(event);
+    let index = names.indexOf(getInputValue())
+    if (index >= 0) {
+        names = names.splice(index, 1);
+    }
+    updateElementsInHtml();
+
 }
 
 function preventDefault(event) {
     event.preventDefault();
-
-    var input = document.querySelector('#name');
-    var div = document.querySelector('.list');
-    var ul = document.createElement('ul');
-    var li = document.createElement('li');
-    var button = document.createElement('button');
-    var text = document.createElement('h5');
-    text.textContent = input.value;
-    button.setAttribute('class', 'btn');
-    button.appendChild(text);
-
-    li.append(button);
-
-    ul.append(li);
-    div.append(ul);
 }
