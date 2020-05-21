b79108b0990e7740ecb9c8cc959e768ad1963196
Felipe Miranda <lipemiranda159@hotmail.com>
2020-05-21 01:07:44 -0300

    use modern js sintaxe

--- Aula9/js/script.js
+++ Aula9/js/script.js
@@ -1,82 +1,64 @@
-window.addEventListener('load', start);
 var names = [];
+let stateChange = null;
+let globalInput = document.querySelector("#name");
+let globalButton = document.querySelector("#change");
+let globalPosition = 0;
+window.addEventListener("load", () => {
+    var form = document.querySelector("form");
+    form.addEventListener("submit", () => {
+        event.preventDefault();
+        !stateChange ? names.push(globalInput.value) : updatePosition();
+        updateElementsInHtml();
+    });
+});
 
-function start() {
-    var form = document.querySelector('form');
-    form.addEventListener('load', preventDefault);
-    form.addEventListener('submit', addNameInArray);
-
+function updatePosition() {
+    names[globalPosition] = globalInput.value;
+    stateChange = false;
+    globalButton.value = "Cadastrar";
 }
 
 function updateElementsInHtml() {
-    function createButton(index) {
-        let button = document.createElement('button');
-        button.classList.add('btn');
-        button.textContent = 'X';
-        button.addEventListener('click', removeNameInArray);
-        button.id = index;
+    function createButton() {
+        let button = document.createElement("button");
+        button.classList.add("btn");
+        button.textContent = "X";
+        button.addEventListener("click", () => {
+            names.splice(event.target.id, 1);
+
+            updateElementsInHtml();
+        });
         return button;
     }
 
-    function createText(value) {
-        let text = document.createElement('h5');
-        text.classList.add('btn-text');
+    function createText(value, index) {
+        let text = document.createElement("h5");
+        text.classList.add("btn-text");
         text.textContent = value;
-        text.addEventListener('click', editValue);
+        text.id = index;
+        text.addEventListener("click", (event) => {
+            globalPosition = event.target.id;
+            let actualLabel = document.getElementById(event.target.id);
+            globalInput.value = actualLabel.textContent;
+            globalButton.value = "Alterar";
+            stateChange = true;
+        });
         return text;
     }
 
-    function removeNameInArray(event) {
-        preventDefault(event);
-        names.splice(event.target.id, 1);
-
-        updateElementsInHtml();
-
-    }
-
-
-    let div = document.querySelector('.list');
-    div.innerHTML = '';
-    let ul = document.createElement('ul');
-
-    for (let index = 0; index < names.length; index++) {
-
-        let li = document.createElement('li');
-        let button = createButton(index);
-        let text = createText(names[index]);
+    let div = document.querySelector(".list");
+    div.innerHTML = "";
+    let ul = document.createElement("ul");
+    let index = 0;
+    names.forEach((name) => {
+        let li = document.createElement("li");
+        let button = createButton();
+        let text = createText(name, index);
         li.append(button);
         li.append(text);
         ul.append(li);
+        index++;
+    });
 
-    }
     div.append(ul);
-
-
-}
-
-function editValue(event) {
-    preventDefault(event);
-    removeNameInArray(event);
-}
-
-function setInputValue(value) {
-    let input = document.querySelector('#name');
-    input.value = value;
-}
-
-function getInputValue() {
-    let input = document.querySelector('#name');
-    return input.value;
-}
-
-function addNameInArray(event) {
-    preventDefault(event);
-    names.push(getInputValue());
-    updateElementsInHtml();
-}
-
-
-
-function preventDefault(event) {
-    event.preventDefault();
 }
--- Trabalho1/js/script.js
+++ Trabalho1/js/script.js
@@ -1,21 +1,21 @@
-window.addEventListener('load', render);
+window.addEventListener("load", render);
 
-let globalRed =  null;
+let globalRed = null;
 let globalGreen = null;
 let globalBlue = null;
 let globalDiv = null;
 
 function getId(label) {
-    let id = '';
+    let id = "";
     switch (label) {
         case "red":
-            text = "red"
+            text = "red";
             break;
         case "green":
-            text = "green"
+            text = "green";
             break;
         case "blue":
-            text = "blue"
+            text = "blue";
             break;
 
         default:
@@ -25,16 +25,16 @@ function getId(label) {
 }
 
 function getSpanText(label) {
-    let text = '';
+    let text = "";
     switch (label) {
         case "red":
-            text = "(R) Vermelho:"
+            text = "(R) Vermelho:";
             break;
         case "green":
-            text = "(G) Verde:"
+            text = "(G) Verde:";
             break;
         case "blue":
-            text = "(B) Azul:"
+            text = "(B) Azul:";
             break;
 
         default:
@@ -44,47 +44,44 @@ function getSpanText(label) {
 }
 
 function getRangeClass(label) {
-    let text = '';
+    let text = "";
     switch (label) {
         case "red":
-            text = "range-red"
+            text = "range-red";
             break;
         case "green":
-            text = "range-green"
+            text = "range-green";
             break;
         case "blue":
-            text = "range-blue"
+            text = "range-blue";
             break;
 
         default:
             break;
     }
     return text;
-
-
 }
 
 function createEdit(label) {
-    let input = document.createElement('input');
-    input.type = 'text';
+    let input = document.createElement("input");
+    input.type = "text";
     input.disabled = true;
     input.value = 0;
-    input.id = getId(label)
-    input.classList.add('number');
+    input.id = getId(label);
+    input.classList.add("number");
     return input;
 }
 
 function getHexValue(value) {
-
-    return value !== '0' ? parseInt(value).toString('16') : '00';
+    return value !== "0" ? parseInt(value).toString("16") : "00";
 }
 
 function getRgbColor() {
     let redValue = getHexValue(globalRed.value);
     let greenValue = getHexValue(globalGreen.value);
-    let blueValue = getHexValue(globalBlue.value);  
-    return '#' + redValue +greenValue
-         +blueValue;
+    let blueValue = getHexValue(globalBlue.value);
+
+    return "#" + redValue + greenValue + blueValue;
 }
 
 function updateDivColor() {
@@ -98,29 +95,29 @@ function createRange(label, edit) {
         updateDivColor();
     }
     //set up
-    let range = document.createElement('input');
-    range.type = 'range';
+    let range = document.createElement("input");
+    range.type = "range";
     range.min = 0;
     range.max = 255;
     range.value = 0;
     range.classList.add(getRangeClass(label));
     //config event
-    range.addEventListener('change', updateEdit);
+    range.addEventListener("change", updateEdit);
     return range;
 }
 
 function createColorDiv() {
-    let divMain = document.createElement('div');
-    divMain.classList.add('main-view')
-    let div = document.createElement('div');
-    div.classList.add('view');
+    let divMain = document.createElement("div");
+    divMain.classList.add("main-view");
+    let div = document.createElement("div");
+    div.classList.add("view");
 
     divMain.appendChild(div);
     return divMain;
 }
 
 function createDiv(label) {
-    let div = document.createElement('div');
+    let div = document.createElement("div");
     div.classList.add(label);
     div.append(createSpan(label));
     let edit = createEdit(label);
@@ -131,19 +128,19 @@ function createDiv(label) {
 }
 
 function createSpan(label) {
-    let span = document.createElement('span');
+    let span = document.createElement("span");
     span.textContent = getSpanText(label);
     return span;
 }
 
 function render() {
-    let optionDiv = document.querySelector('.options');
-    optionDiv.appendChild(createDiv('red'));
-    optionDiv.appendChild(createDiv('green'));
-    optionDiv.appendChild(createDiv('blue'));
+    let optionDiv = document.querySelector(".options");
+    optionDiv.appendChild(createDiv("red"));
+    optionDiv.appendChild(createDiv("green"));
+    optionDiv.appendChild(createDiv("blue"));
     optionDiv.appendChild(createColorDiv());
-    globalRed = document.querySelector('#red');
-    globalGreen = document.querySelector('#green');
-    globalBlue = document.querySelector('#blue');
-    globalDiv = document.querySelector('.view');
+    globalRed = document.querySelector("#red");
+    globalGreen = document.querySelector("#green");
+    globalBlue = document.querySelector("#blue");
+    globalDiv = document.querySelector(".view");
 }
