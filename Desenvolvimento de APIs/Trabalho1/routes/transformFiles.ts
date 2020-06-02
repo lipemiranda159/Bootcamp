9967b602e36725cb62ffa67931b9c9289a3e59c4
Felipe <felipem@take.net>
2020-06-02 19:17:21 -0300

    ident code

--- Desenvolvimento de APIs/Trabalho1/routes/transformFiles.ts
+++ Desenvolvimento de APIs/Trabalho1/routes/transformFiles.ts
@@ -5,26 +5,27 @@ const readFile = promises.readFile;
 const writeFile = promises.writeFile;
 export const transfromFileRouter = express.Router();
 
-var errorHandler = function (req: express.Request, res: express.Response, next: NextFunction) {
-  console.log('LOGGED');
-  try{
-    next(); 
-  }
-  catch(err){
+var errorHandler = function (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) {
+  console.log("LOGGED");
+  try {
+    next();
+  } catch (err) {
     console.log(err.message);
   }
 };
 
 transfromFileRouter.use(errorHandler);
 
-
-
 transfromFileRouter.get("/generateFiles", async (_, res) => {
   const stateData = await readFile("./files/Estados.json", "utf8");
-  const cityData = await readFile("./files/Cidades.json","utf8");
+  const cityData = await readFile("./files/Cidades.json", "utf8");
   const datas = JSON.parse(stateData);
-  
-  res.send("Ok");  
+
+  res.send("Ok");
 });
 
 transfromFileRouter.get("/:code", (req, res) => {});
