72f49f655ff2e46ba3d3cb38caddb00ac6cd4049
Felipe <felipem@take.net>
2020-06-02 21:26:49 -0300

    add city router in app.ts

--- Desenvolvimento de APIs/Trabalho1/app.ts
+++ Desenvolvimento de APIs/Trabalho1/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDoc from "./swagger.json";
-import {transfromFileRouter} from "./routes/transformFiles";
+import { transfromFileRouter } from "./routes/transformFiles";
+import { citiesRouter } from "./routes/cities";
 
 var app = express();
 app.use(express.json());
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
- app.use("/transformFile", transfromFileRouter);
+app.use("/transformFile", transfromFileRouter);
+app.use("/cities", citiesRouter);
 
 app.listen(3000, function () {
   console.log("started api!");
