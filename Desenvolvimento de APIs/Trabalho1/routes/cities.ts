9a43d181fc6b6f628bfd2a4a7710629767e6106c
Felipe <felipem@take.net>
2020-06-02 22:23:12 -0300

    update cities router name

--- Desenvolvimento de APIs/Trabalho1/routes/cities.ts
+++ Desenvolvimento de APIs/Trabalho1/routes/cities.ts
@@ -6,7 +6,13 @@ const processedPath = "./files/processed";
 const encoding = "utf8";
 
 export const citiesRouter = express.Router();
-
+/**
+ * @route GET /cities/:uf
+ * @group Cities - Generate api files
+ * @param {string} uf.path.required
+ * @returns {Number} 200 - Count of cities of UF
+ * @returns {Error}  default - Unexpected error
+ */
 citiesRouter.get("/:uf", async (req, res) => {
   let { uf } = req.params;
   let city = readFile(`${processedPath}/${uf}.json`, encoding);
