7a64e8b7550e2ce5d65730047d75fe6a7e8a368d
Felipe <felipem@take.net>
2020-06-03 22:33:30 -0300

    implement return number of cities

--- Desenvolvimento de APIs/Trabalho1/routes/cities.ts
+++ Desenvolvimento de APIs/Trabalho1/routes/cities.ts
@@ -9,8 +9,8 @@ export const citiesRouter = express.Router();
 
 citiesRouter.get("/:uf", async (req, res) => {
   let { uf } = req.params;
-  let city = readFile(`${processedPath}/${uf}.json`, encoding);
-  console.log(city);
+  let city = await readFile(`${processedPath}/${uf}.json`, encoding);
+  res.status(200).send({ length: city.length });
 });
 
 export default citiesRouter;
