05eac466bf13be365df8b7bebc0c3a441ab66326
Felipe <felipem@take.net>
2020-06-04 00:28:49 -0300

    add new route top five states

--- Desenvolvimento de APIs/Trabalho1/routes/cities.ts
+++ Desenvolvimento de APIs/Trabalho1/routes/cities.ts
@@ -6,5 +6,6 @@ export const citiesRouter = express.Router();
 const citiesController = new CitiesController();
 
 citiesRouter.get("/:uf", citiesController.getUfCitiesLength);
+citiesRouter.get("/count/topFiveStates", citiesController.getTopFiveStates);
 
 export default citiesRouter;
