67a1f5c1e61e032c04446459794fcbd275629d03
Felipe <felipem@take.net>
2020-06-03 23:19:05 -0300

    create constants file

---
+++ Desenvolvimento de APIs/Trabalho1/Constants.ts
@@ -0,0 +1,6 @@
+export default class Constants {
+  public static readonly stateFile = "./files/Estados.json";
+  public static readonly cityFile = "./files/Cidades.json";
+  public static readonly processedPath = "./files/processed";
+  public static readonly encoding = "utf8";
+}
