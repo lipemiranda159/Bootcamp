96a5836f0a311496f3af782bf8a20122ca948718
Felipe <felipem@take.net>
2020-06-04 00:28:31 -0300

    handle exceptions and implement top five states

--- Desenvolvimento de APIs/Trabalho1/controllers/citiesController.ts
+++ Desenvolvimento de APIs/Trabalho1/controllers/citiesController.ts
@@ -5,13 +5,35 @@ const filesPath = "./files";
 
 class CitiesController {
   async getUfCitiesLength(request: Request, response: Response) {
-    let { uf } = request.params;
-    let city = await FileService.getFileData(`${processedPath}/${uf}.json`);
-    response.status(200).send({ length: city.length });
+    try {
+      let { uf } = request.params;
+      let city = await FileService.getFileData(`${processedPath}/${uf}.json`);
+      response.status(200).send({ length: city.length });
+    } catch (err) {
+      response.status(500).send({ error: err });
+    }
   }
 
   async getTopFiveStates(request: Request, response: Response) {
-    let states = FileService.getFileData(`${filesPath}/Estados.json`);
+    try {
+      let states = await FileService.getFileData(`${filesPath}/Estados.json`);
+      let statesCities: any[] = [];
+      const promises = states.map(async (state: any) => {
+        let cities = await FileService.getFileData(
+          `${processedPath}/${state.Sigla}.json`
+        );
+        statesCities.push({ Uf: state.Sigla, CountCities: cities.length });
+      });
+      await Promise.all(promises);
+      statesCities = statesCities
+        .sort(function (a, b) {
+          return b.CountCities - a.CountCities;
+        })
+        .slice(0, 5);
+      response.status(200).send({ teste: statesCities });
+    } catch (err) {
+      response.status(500).send({ error: err });
+    }
   }
 }
 
