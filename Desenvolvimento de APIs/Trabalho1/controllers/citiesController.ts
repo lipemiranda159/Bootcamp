597134b5e108c33007bac8ecee38a543e58e5050
Felipe <felipem@take.net>
2020-06-06 01:01:21 -0300

    add order by methods

--- Desenvolvimento de APIs/Trabalho1/controllers/citiesController.ts
+++ Desenvolvimento de APIs/Trabalho1/controllers/citiesController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import FileService from "../Services/FileService";
 import ProcessFileService from "../Services/ProcessFileService";
 import ArrayOrderService from "../Services/ArrayOrderService";
+import Constants from "../Constants";
+import UtilService from "../Services/UtilsService";
 const processedPath = "./files/processed";
 
 class CitiesController {
@@ -11,35 +13,103 @@ class CitiesController {
       let city = await FileService.getFileData(`${processedPath}/${uf}.json`);
       response.status(200).send({ length: city.length });
     } catch (err) {
-      response.status(500).send({ error: err });
+      response.status(500).send({ error: err.stack });
     }
   }
 
   async getTopFiveStates(request: Request, response: Response) {
     try {
-      let statesCities = await ProcessFileService.getCitesCount();
-      statesCities = ArrayOrderService.orderByDescending(statesCities).slice(
-        0,
-        5
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Descending
       );
-      response.status(200).send({ response: statesCities });
+      statesCities = ArrayOrderService.orderByNumberDescending(statesCities);
+
+      let result = await UtilService.returnTopFive(statesCities);
+      response.status(200).send({ response: result });
     } catch (err) {
-      response.status(500).send({ error: err });
+      response.status(500).send({ error: err.stack });
     }
   }
 
   async getLastFiveStates(request: Request, response: Response) {
     try {
-      let statesCities = await ProcessFileService.getCitesCount();
-      statesCities = ArrayOrderService.orderByAscending(statesCities).slice(
-        0,
-        5
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Descending
       );
-      response
-        .status(200)
-        .send({ response: ArrayOrderService.orderByDescending(statesCities) });
+      statesCities = ArrayOrderService.orderByNumberAscending(statesCities);
+      let result = await UtilService.returnTopFive(statesCities);
+
+      response.status(200).send({
+        response: ArrayOrderService.orderByNumberDescending(result),
+      });
     } catch (err) {
-      response.status(500).send({ error: err });
+      response.status(500).send({ error: err.stack });
+    }
+  }
+
+  async getOrderedDescNames(request: Request, response: Response) {
+    try {
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Descending
+      );
+      let orderedList = await UtilService.getCitiesData(statesCities);
+      let result: any[] = [];
+      orderedList.forEach((data: any) => {
+        let orderedData = ArrayOrderService.orderByStringDescending(data);
+        result.push(orderedData[0]);
+      });
+      response.status(200).send(result);
+    } catch (err) {
+      response.status(500).send({ error: err.stack });
+    }
+  }
+
+  async getOrderedDescName(request: Request, response: Response) {
+    try {
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Descending
+      );
+      let result = ArrayOrderService.orderByStringDescending(
+        await UtilService.getCityData(statesCities)
+      );
+
+      response.status(200).send(result[0]);
+    } catch (err) {
+      response.status(500).send({ error: err.stack });
+    }
+  }
+
+  async getOrderedAscNames(request: Request, response: Response) {
+    try {
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Ascending
+      );
+      let orderedList = await UtilService.getCitiesData(statesCities);
+      let result: any[] = [];
+      orderedList.forEach((data: any) => {
+        let orderedData = ArrayOrderService.orderByStringAscending(data);
+        result.push(orderedData[0]);
+      });
+
+      response.status(200).send(result);
+    } catch (err) {
+      response.status(500).send({ error: err.stack });
+    }
+  }
+
+  async getOrderedAscName(request: Request, response: Response) {
+    console.log("test");
+    try {
+      let statesCities = await ProcessFileService.getCitesCount(
+        Constants.Ascending
+      );
+      let result = ArrayOrderService.orderByStringAscending(
+        await UtilService.getCityData(statesCities)
+      );
+
+      response.status(200).send(result[0]);
+    } catch (err) {
+      response.status(500).send({ error: err.stack });
     }
   }
 }
--- Desenvolvimento de APIs/Trabalho1/routes/cities.ts
+++ Desenvolvimento de APIs/Trabalho1/routes/cities.ts
@@ -8,4 +8,13 @@ const citiesController = new CitiesController();
 citiesRouter.get("/:uf", citiesController.getUfCitiesLength);
 citiesRouter.get("/count/topFiveStates", citiesController.getTopFiveStates);
 citiesRouter.get("/count/LastFiveStates", citiesController.getLastFiveStates);
+citiesRouter.get(
+  "/orderedDesc/cityNames",
+  citiesController.getOrderedDescNames
+);
+citiesRouter.get("/orderedDesc/cityName", citiesController.getOrderedDescName);
+
+citiesRouter.get("/orderedAsc/cityNames", citiesController.getOrderedAscNames);
+citiesRouter.get("/orderedAsc/cityName", citiesController.getOrderedAscName);
+
 export default citiesRouter;
