7b4711fcc0d0ef451ad06677965adb5c750e1b41
Felipe <felipem@take.net>
2020-06-06 01:00:55 -0300

    add ascending and descending const

--- Desenvolvimento de APIs/Trabalho1/Constants.ts
+++ Desenvolvimento de APIs/Trabalho1/Constants.ts
@@ -3,4 +3,6 @@ export default class Constants {
   public static readonly cityFile = "./files/Cidades.json";
   public static readonly processedPath = "./files/processed";
   public static readonly encoding = "utf8";
+  public static readonly Ascending = 0;
+  public static readonly Descending = 1;
 }
