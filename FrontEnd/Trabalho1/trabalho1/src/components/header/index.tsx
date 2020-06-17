567df0f2141f725612d4f8911a313f457e5bbeae
Felipe <felipem@take.net>
2020-06-16 22:58:17 -0300

    create header component

---
+++ FrontEnd/Trabalho1/trabalho1/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./index.module.css";
+function Header() {
+  return (
+    <header className={css.mainHeader}>
+      <h1>React Salário</h1>
+    </header>
+  );
+}
+
+export default Header;
