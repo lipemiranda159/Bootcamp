67de194c8018f09e8b26992b7dc30003f1d1297c
Felipe <felipem@take.net>
2020-06-16 22:58:40 -0300

    install components

--- FrontEnd/Trabalho1/trabalho1/src/App.tsx
+++ FrontEnd/Trabalho1/trabalho1/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header";
+import InputValue from "./components/input";
 
 function App() {
-  return <h1>Trabalho 1</h1>;
+  return (
+    <div className="main">
+      <Header />
+      <div className="row">
+        <InputValue id="salary" type="number" disabled={false} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
