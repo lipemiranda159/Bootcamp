6bc70d87cd039d730d01a7f49bf5791855903e64
Felipe <felipem@take.net>
2020-06-16 23:37:25 -0300

    create component in incex view

--- FrontEnd/Trabalho1/trabalho1/src/App.tsx
+++ FrontEnd/Trabalho1/trabalho1/src/App.tsx
@@ -8,7 +8,64 @@ function App() {
     <div className="main">
       <Header />
       <div className="row">
-        <InputValue id="salary" type="number" disabled={false} />
+        <div className="col s12">
+          <div className="input-field col s12">
+            <InputValue
+              id="salary"
+              type="number"
+              disabled={false}
+              text="Salário Bruto"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <div className="input-field col s3">
+              <InputValue
+                id="BaseInss"
+                type="text"
+                disabled={true}
+                text="Base INSS"
+              />
+            </div>
+            <div className="input-field col s3">
+              <InputValue
+                id="DescInss"
+                type="text"
+                disabled={true}
+                text="Deconto INSS"
+              />
+            </div>
+            <div className="input-field col s3">
+              <InputValue
+                id="BaseIprf"
+                type="text"
+                disabled={true}
+                text="Base IPRF"
+              />
+            </div>
+            <div className="input-field col s3">
+              <InputValue
+                id="DescIprf"
+                type="text"
+                disabled={true}
+                text="Desconto IPRF"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <div className="input-field col s3">
+            <InputValue
+              id="SalaryLiq"
+              type="text"
+              disabled={true}
+              text="Salário Líquido"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- FrontEnd/Trabalho1/trabalho1/src/components/input/index.tsx
+++ FrontEnd/Trabalho1/trabalho1/src/components/input/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import css from "./index.module.css";
 
-const InputValue: React.FC<{ id: string; type: string; disabled: boolean }> = ({
-  id,
-  type,
-  disabled,
-}) => {
+const InputValue: React.FC<{
+  id: string;
+  type: string;
+  disabled: boolean;
+  text: string;
+}> = ({ id, type, disabled, text }) => {
   return (
     <fieldset className={css.fieldset}>
       <label htmlFor={id} className={css.label}>
-        Salário Bruto
+        {text}
       </label>
       <input id={id} type={type} disabled={disabled} />
     </fieldset>
