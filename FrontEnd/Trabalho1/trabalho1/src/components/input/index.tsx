d123ca7919b67a91d5939a3fdadd1a01c3e27d0c
Felipe <felipem@take.net>
2020-06-18 00:11:24 -0300

    change colors inputs

--- FrontEnd/Trabalho1/trabalho1/src/components/input/index.tsx
+++ FrontEnd/Trabalho1/trabalho1/src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from "./index.module.css";
 
 const InputValue: React.FC<{
@@ -8,12 +8,30 @@ const InputValue: React.FC<{
   text: string;
   value: string;
 }> = ({ id, type, disabled, text, value }) => {
+  useEffect(() => {
+    if (value !== "") {
+      const input = document.getElementById(id);
+      if (input) {
+        if (id === "DescInss") {
+          input.style.color = "#e67e22";
+        } else if (id === "DescIprf") {
+          input.style.color = "#c0392b";
+        }
+      }
+    }
+  });
   return (
     <fieldset className={css.fieldset}>
       <label htmlFor={id} className={css.label}>
         {text}
       </label>
-      <input id={id} type={type} disabled={disabled} value={value} />
+      <input
+        className={css.inputvalue}
+        id={id}
+        type={type}
+        disabled={disabled}
+        value={value}
+      />
     </fieldset>
   );
 };
