4b1ea2750f199266361ecc7a85cfc841a047f2ef
lipemiranda159 <lipemiranda159@hotmail.com>
2020-06-30 23:25:07 -0300

    use controller

--- Modulo4/Trabalho1/app.ts
+++ Modulo4/Trabalho1/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import { balanceRouter } from "./routes/balanceRouter";
 
 const app = express();
+app.use(express.json());
+
+app.use("/balance", balanceRouter);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port);
