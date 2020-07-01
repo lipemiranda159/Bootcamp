8547a0114e7dbe02b62a04c9468636f5c911ef42
lipemiranda159 <lipemiranda159@hotmail.com>
2020-06-30 23:24:57 -0300

    create database service

---
+++ Modulo4/Trabalho1/services/databaseService.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from "mongoose";
+
+class databaseService {
+  private schema: Schema;
+  private createdModel = false;
+  private dbContext: any;
+
+  constructor() {
+    this.schema = new Schema({
+      agencia: {
+        type: Number,
+        required: true,
+      },
+      conta: {
+        type: Number,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      balance: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    });
+  }
+
+  public async connect() {
+    try {
+      const uri =
+        "mongodb+srv://mongo:260191@cluster0-kvtie.gcp.mongodb.net/Igti?retryWrites=true&w=majority";
+
+      await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      if (!this.createdModel) {
+        this.dbContext = mongoose.model("Bank", this.schema, "Bank");
+        this.createdModel = true;
+      }
+    } catch (error) {
+      console.log(`err: ${error}`);
+      throw error;
+    }
+  }
+
+  public async getBalances() {
+    try {
+      return await this.dbContext.find({});
+    } catch (error) {
+      return error;
+    }
+  }
+
+  public closeConnection() {
+    this.dbContext.closeConnection();
+  }
+}
+
+export default databaseService;
