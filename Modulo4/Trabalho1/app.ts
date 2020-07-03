181fd05d7831a87e360c7cd9549df47f89be959d
lipemiranda159 <lipemiranda159@hotmail.com>
2020-07-02 22:47:23 -0300

    configure swagger

--- Modulo4/Trabalho1/app.ts
+++ Modulo4/Trabalho1/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { balanceRouter } from "./routes/balanceRouter";
+import * as swagger_ui from "swagger-ui-express";
+import * as swagger_document from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
+// Starting swagger
+app.use("/api/v1/docs", swagger_ui.serve, swagger_ui.setup(swagger_document));
 app.use("/balance", balanceRouter);
 
 const port = process.env.PORT || 3000;
